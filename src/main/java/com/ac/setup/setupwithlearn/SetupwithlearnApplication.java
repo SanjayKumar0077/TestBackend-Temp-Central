002516cf7fd90ac1719edf89d8ca8beac9111d59
Sanjay <ks0077sanjay.com>
2023-11-26 06:53:58 +0530

    back end Project

---
+++ src/main/java/com/ac/setup/setupwithlearn/SetupwithlearnApplication.java
@@ -0,0 +1,13 @@
+package com.ac.setup.setupwithlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupwithlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetupwithlearnApplication.class, args);
+	}
+
+}
